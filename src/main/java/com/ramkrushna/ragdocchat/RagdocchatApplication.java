d2970f6934ba62f97d9d6590b30039276115888f
User <user@example.com>
2025-12-14 18:56:16 +0530

    Initial commit

---
+++ src/main/java/com/ramkrushna/ragdocchat/RagdocchatApplication.java
@@ -0,0 +1,13 @@
+package com.ramkrushna.ragdocchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagdocchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagdocchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramkrushna/ragdocchat/RagdocchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramkrushna.ragdocchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagdocchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
